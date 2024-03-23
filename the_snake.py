73efa10c2fe2f201ddfc97fc6731ef33f5892f01
Egor Semochkin <yegorchik585@yandex.ru>
2024-03-23 22:57:34 +0300

    исправление метода draw в классе Apple

--- the_snake.py
+++ the_snake.py
@@ -68,7 +68,10 @@ class Apple(GameObject):
         rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(screen, self.body_color, rect)
         pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
-         
+        
+        if self.last:
+            last_rect = pygame.Rect(self.last, (GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
 
 class Snake(GameObject):
     def __init__(self) -> None:
